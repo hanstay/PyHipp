c5f21a165b6359d28fb766ae11cdf57ebdfac356
hanstay <hanstayhe@gmail.com>
2021-11-10 14:40:43 +0800

    Update rplparallel-slurm.sh

--- rplparallel-slurm.sh
+++ rplparallel-slurm.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 # Submit this script with: sbatch <this-filename>
-
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-<<<<<<< HEAD
-#SBATCH -J "rplpl"   # job name
-=======
-<<<<<<< HEAD:myrse-slurm.sh
 #SBATCH --cpus-per-task=1 #number of cpus for this task
-#SBATCH -J "rse"   # job name
-=======
 #SBATCH -J "rplpl"   # job name
->>>>>>> upstream/main:rplparallel-slurm.sh
->>>>>>> upstream/main
+
 
 ## /SBATCH -p general # partition (queue)
 #SBATCH -o rplpl-slurm.%N.%j.out # STDOUT
@@ -36,29 +28,7 @@ pyh.aligning_objects();\
 pyh.raycast(1);\
 print(time.localtime());\
 print(time.time()-t0);"
-=======
-<<<<<<< HEAD:myrse-slurm.sh
-=======
-import DataProcessingTools as DPT; \
-import os; \
->>>>>>> upstream/main:rplparallel-slurm.sh
-import time; \
-import os; \
-t0 = time.time(); \
-print(time.localtime()); \
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLParallel, saveLevel=1); \
-DPT.objects.processDirs(dirs=None, objtype=pyh.Unity, saveLevel=1); \
-pyh.EDFSplit(); \
-os.chdir('session01'); \
-pyh.aligning_objects(); \
-pyh.raycast(1); \
-print(time.localtime()); \
-<<<<<<< HEAD:myrse-slurm.sh
-print(time.time()-t0);"
-=======
-print(time.time()-t0);"
 
 aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RPLParallelJobDone"
 
->>>>>>> upstream/main:rplparallel-slurm.sh
->>>>>>> upstream/main
+
