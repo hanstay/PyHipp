e83f8f8d8006d51bd36fc46bfccbb80b01c252c2
hanstay <hanstayhe@gmail.com>
2021-11-10 14:40:00 +0800

    Update myrse-slurm.sh

--- myrse-slurm.sh
+++ myrse-slurm.sh
@@ -5,12 +5,8 @@
 #SBATCH --time=24:00:00   # walltime
 #SBATCH --ntasks=1   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
-<<<<<<< HEAD:myrse-slurm.sh
 #SBATCH --cpus-per-task=1 #number of cpus for this task
 #SBATCH -J "rse"   # job name
-=======
-#SBATCH -J "rplpl"   # job name
->>>>>>> upstream/main:rplparallel-slurm.sh
 
 ## /SBATCH -p general # partition (queue)
 #SBATCH -o rplpl-slurm.%N.%j.out # STDOUT
@@ -18,11 +14,6 @@
 
 # LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
 python -u -c "import PyHipp as pyh; \
-<<<<<<< HEAD:myrse-slurm.sh
-=======
-import DataProcessingTools as DPT; \
-import os; \
->>>>>>> upstream/main:rplparallel-slurm.sh
 import time; \
 import os; \
 t0 = time.time(); \
@@ -34,11 +25,4 @@ os.chdir('session01'); \
 pyh.aligning_objects(); \
 pyh.raycast(1); \
 print(time.localtime()); \
-<<<<<<< HEAD:myrse-slurm.sh
 print(time.time()-t0);"
-=======
-print(time.time()-t0);"
-
-aws sns publish --topic-arn arn:aws:sns:ap-southeast-1:018084650241:awsnotify --message "RPLParallelJobDone"
-
->>>>>>> upstream/main:rplparallel-slurm.sh
