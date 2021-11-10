7441f7202f5ec9209d6e64b56c6a24c8f76568ce
hanstay <hanstayhe@gmail.com>
2021-11-10 14:44:30 +0800

    Update mypipeline-slurm.sh

--- mypipeline-slurm.sh
+++ mypipeline-slurm.sh
@@ -18,13 +18,10 @@ import os; \
 import time; \
 t0 = time.time(); \
 print(time.localtime()); \
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLParallel, saveLevel=1);
-\
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[9, 31,
-34, 56, 72, 93, 119, 120]); \
+DPT.objects.processDirs(dirs=None, objtype=pyh.RPLParallel, saveLevel=1);\
+DPT.objects.processDirs(dirs=None, objtype=pyh.RPLSplit, channel=[9,31,34,56,72,93,119,120]); \
 DPT.objects.processDirs(dirs=None, objtype=pyh.RPLLFP, saveLevel=1); \
-DPT.objects.processDirs(dirs=None, objtype=pyh.RPLHighPass, saveLevel=1);
-\
+DPT.objects.processDirs(dirs=None, objtype=pyh.RPLHighPass, saveLevel=1);\
 DPT.objects.processDirs(dirs=None, objtype=pyh.Unity, saveLevel=1); \
 pyh.EDFSplit(); \
 os.chdir('session01'); \
